2d327767ef8c256779234706f70cb5211a454459
Artem Papeta <ar.papeta@gmail.com>
2023-02-18 15:08:17 +0200

    Add config file with helper implementation.

---
+++ DataProcessing/ConfigHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DataProcessing;
+
+public static class ConfigHelper
+{
+    private static readonly IConfiguration _configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).Build();
+
+    public static string FolderAPath => _configuration.GetSection("FolderAPath").Value!;
+    public static string FolderBPath => _configuration.GetSection("FolderBPath").Value!;
+
+}
--- DataProcessing/Program.cs
+++ DataProcessing/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using DataProcessing;
+
+string folderPath = ConfigHelper.FolderAPath;
+System.IO.Directory.CreateDirectory(folderPath);
+
+string workingDirectory = Environment.CurrentDirectory;
+Console.WriteLine(workingDirectory);
